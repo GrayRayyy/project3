b4b3ca9169fc695a2e64d784c095cd4dd3dc1291
GrayRayyy <66657695+GrayRayyy@users.noreply.github.com>
2020-06-09 15:37:02 +1200

    Update speedDecider.hpp

--- speedDecider.hpp
+++ speedDecider.hpp
@@ -1,6 +1,8 @@
-void setMotorCore(int* pixels){
-    double vLeft = 40.0;
-    double vRight = 30.0;
-	setMotors(vLeft,vRight);
-    std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;   
+#include "robot.cpp";
+public void MotorCore(int*pixels){
+setSpeed();      
+double vLeft = 50.0;        // Set speed of left robot motor
+double vRight = 30.0;       // Set speed of right robot motor
+setMotors(vLeft, vRight);   // Set position of the motors
+sttd::cout<<" vLeft="<<vLeft<<" vRight="<<vRight<<std::endl;
 }
