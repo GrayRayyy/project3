4935e90fa2e490a5445b792b95c741013dde2a9d
GrayRayyy <66657695+GrayRayyy@users.noreply.github.com>
2020-06-12 13:10:48 +1200

    Update and rename speedDecider.hpp to Robot.commit1

--- speedDecider.hpp
+++
@@ -1,8 +0,0 @@
-#include "robot.cpp";
-public void MotorCore (int*pixels){
-setSpeed();
-double vLeft = 50.0;           // Left robot motor
-double vRight = 30.0;          // Right robot motor
-setMotors(vLeft, vRight);      // Set position of the motors
-sttd::cout<<" vLeft="<<vLeft<<" vRight="<<vRight="<<vRight<<std::endl;
-      }
